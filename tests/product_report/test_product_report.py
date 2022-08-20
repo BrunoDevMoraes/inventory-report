f01c22c0b4c1b7fd767561eac6a214fd32463034
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-08-20 19:05:02 -0300

    add 02

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,20 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        100,
+        'Batata',
+        'BatataCompany',
+        '01/02',
+        '01/03', '0MB99',
+        'Guardar'
+    )
+
+    assert str(product) == (
+        f"O produto {product.nome_do_produto}"
+        f" fabricado em {product.data_de_fabricacao}"
+        f" por {product.nome_da_empresa} com validade"
+        f" até {product.data_de_validade}"
+        f" precisa ser armazenado {product.instrucoes_de_armazenamento}."
+    )
