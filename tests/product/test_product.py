3decaf441cecd0e376ea1e34d3997aa656013138
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-08-20 18:34:37 -0300

    add 01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,26 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(100, 'Batata', 'BatataCompany', '01/02', '01/03', '0MB99', 'Guardar em temperatura ambiente')
+
+    assert type(product.id) == int
+    assert product.id == 100
+
+    assert type(product.nome_do_produto) == str
+    assert product.nome_do_produto == 'Batata'
+
+    assert type(product.nome_da_empresa) == str
+    assert product.nome_da_empresa == 'BatataCompany'
+
+    assert type(product.data_de_fabricacao) == str
+    assert product.data_de_fabricacao == '01/02'
+
+    assert type(product.data_de_validade) == str
+    assert product.data_de_validade == '01/03'
+
+    assert type(product.numero_de_serie) == str
+    assert product.numero_de_serie == '0MB99'
+
+    assert type(product.instrucoes_de_armazenamento) == str
+    assert product.instrucoes_de_armazenamento == 'Guardar em temperatura ambiente'
