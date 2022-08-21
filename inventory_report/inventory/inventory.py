b0ec8026a9ed2cd3b38116a632360636707723e6
BrunoDevMoraes <brunodevmoraes@gmail.com>
2022-08-21 15:29:17 -0300

    add 06

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,13 +1,20 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
     @staticmethod
     def import_data(path: str, type: str) -> str:
-        with open(path, 'r') as csvfile:
-            file_data = csv.DictReader(csvfile)
+        with open(path, 'r') as file:
+            file_data = dict()
+            if path.lower().endswith('.csv'):
+                file_data = csv.DictReader(file)
+            if path.lower().endswith('.json'):
+                file_data = json.load(file)
+            if path.lower().endswith('.xml'):
+                file_data = csv.DictReader(file)
             data_list = [row for row in file_data]
             if type == "simples":
                 return SimpleReport.generate(data_list)
